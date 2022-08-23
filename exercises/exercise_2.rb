9000f9d3c28cab978962a5921221c97bc89f68a8
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-08-23 01:15:34 +0000

    complete ex 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+puts @store1.annual_revenue
+puts @store2.annual_revenue
+
+@store1.update(name: 'North Vancouver')
